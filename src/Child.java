eadd04f1983e08d1bff9b29f60fde254c54f34d6
DESKTOP-OHBVMKI\admin <yuhiee123@gmail.com>
2022-10-07 16:32:17 +0900

    20221007

---
+++ src/Child.java
@@ -0,0 +1,9 @@
+public class Child extends Parent{
+    @Override
+    public void method2(){
+        System.out.println("child method()");
+    }
+    public void method3(){
+        System.out.println("child method3()");
+    }
+}
